9fc9e947f5892ed9a14f3a8625c4af97e52fda56
Jeho Yoo <jay@cnai.ai>
2024-04-22 09:41:41 +0900

    UPDATE: 설정 파일 변경
    - Dev 모드 실행 시 fetch 함수 로깅 기능 추가
    - Production 모드 실행 시 pm2 log에 timestamp 추가
    - docker 실행 시 타임존 한국시간으로 변경
    - eslint 설정 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -13,7 +13,6 @@ const preview: Preview = {
   },
   parameters: {
     i18n,
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: "next-app",
       script: "node_modules/next/dist/bin/next",
+      time: true,
       args: "start --port 3000",
       instances: 2,
       exec_mode: "cluster",
